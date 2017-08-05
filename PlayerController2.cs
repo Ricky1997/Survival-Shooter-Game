53d5c5493b74a6e81c38e66657f9da08f82de4a0
Harshit Bansal <30345507+Ricky1997@users.noreply.github.com>
2017-08-05 23:27:05 +0530

    Add files via upload

---
+++ PlayerController2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController2 : MonoBehaviour
+{
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			Fire();
+		}
+	}
+
+
+	void Fire()
+	{
+		if (bulletPrefab.activeInHierarchy==true) {
+			return;
+		}
+		bulletPrefab.SetActive (true);
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 9;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 1.5f);        
+		bulletPrefab.SetActive (false);
+	}
+}
